b1c26bbc0bc79335fee4be500e3ef62259b992a9
Devon Zhen <zhen.devon@gmail.com>
2020-05-04 17:48:20 -0400

    Update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, SystemJsNgModuleLoader } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
+import { RestfulService } from './../services/restful.service';
+import { SharedDatService } from 
 
 @Component({
   selector: 'app-login',
@@ -12,12 +14,9 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private route: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private restService: RestfulService
   ) { }
-
-  ngOnInit(): void {
-    this.LoginForm();
-  }
 
   LoginForm(){
     this.loginForm=this.formBuilder.group({
@@ -30,12 +29,13 @@ export class LoginComponent implements OnInit {
   get username(){
     return this.loginForm.get('username');
   }
-
+  //Validation Only
   get password(){
     return this.loginForm.get('password');
   }
 
   storage(username:any, password:any){
+    
     console.log("Inside Storage Function:  ");
     console.log("Initial Username: "+JSON.stringify(username)+" Initial Password: "+JSON.stringify(password));
     sessionStorage.setItem("username", username);
@@ -52,9 +52,10 @@ export class LoginComponent implements OnInit {
     let user = sessionStorage.getItem("username");
     let pass = sessionStorage.getItem("password");
     console.log("Action user: "+user +" Action pass: "+pass);
+    
     if(user == 'devon' && pass == 'zhen'){
       document.getElementById("navbarNavAltMarkup").style.visibility  = "visible";
-      this.route.navigate(['/home']);
+      this.route.navigate(['/home']).then(() => {location.reload()});
       
     }
     else{
@@ -62,8 +63,43 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  //Check Username and Password validation
+  getUserPass(username:any, password:any){
+    this.restService.getUsernamePassword(username,password).subscribe(
+      data => { 
+        console.log("Load Details: "+JSON.stringify(data));
+        if(data.username==null && data.password==null || data.username=='' && data.password==''){
+          console.log("Login Failed")
+        }
+        else{
+          sessionStorage.setItem("username", username);
+          sessionStorage.setItem("password", password);
+        }
+      },
+      err => {
+        console.log("Error occured: getAccountForm() Failed")
+      }
+    );
+  }
+
+
+  ngOnInit(): void {
+    this.LoginForm();
+    let user = sessionStorage.getItem("username");
+    let pass = sessionStorage.getItem("password");
+    console.log("User: "+user+ "Pass: "+pass);
+    if(user!=null){
+      // this.route.navigate(['header']);
+      document.getElementById("mainBody").style.visibility  = "visible";
+    }
+  }
+
+
+
   onSubmit() {
     console.log("Login Form Values: "+JSON.stringify(this.loginForm.value));
+    //Check User/Pass in DB & stores in SessionStorage
+    this.getUserPass(this.loginForm.get('username').value, this.loginForm.get('password').value);
     //Stores Login Values into SessionStorage
     this.storage(this.loginForm.get('username').value, this.loginForm.get('password').value);
     //Takes action to Route and unhide elements
